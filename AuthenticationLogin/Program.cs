748a74968c617127547b9c5badccc53b843d474b
Sarah <mahlangusarah8@gmail.com>
2018-05-22 15:10:52 +0200

    I added a comment

--- AuthenticationLogin/Program.cs
+++ AuthenticationLogin/Program.cs
@@ -17,6 +17,8 @@ namespace AuthenticationLogin
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            ///Im making change to the project
         }
     }
 }
